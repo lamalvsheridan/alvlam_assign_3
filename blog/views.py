6965dbffa79cd2233a69b6c777678a1766be1c59
Alvin Lam <105746805+lamalvsheridan@users.noreply.github.com>
2022-11-08 23:19:52 -0500

    Cleanup
    Delete some loose code

--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,7 @@ def home(request):
     """
     The Blog homepage
     """
-    # Get last 3 posts
-    latest_posts = models.Post.objects.published()
-    # Add as context variable "latest_posts"
-    context = {'latest_posts': latest_posts}
     return render(
         request,
-        'blog/home.html',
-        context
+        'blog/home.html'
     )
